42b458216154b77531e7f6148eb7e87463a22f1e
Abhishek <abhibhardwaj1017@gmail.com>
2025-07-26 23:26:44 +0530

    feat: API docs

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { API_PREFIX, APP_NAME } from './constants';
 
 @Injectable()
 export class AppService {
   getHello() {
     const data = {
       success: true,
-      message:
-        'DIGIPIN API is running and API docs can be found at at /api/v1/api-docs',
+      message: `${APP_NAME} API is running and API docs can be found at ${API_PREFIX}/api-docs`,
       timestamp: Date.now(),
     };
-    return data
+    return data;
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { API_PREFIX, APP_NAME } from './constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.setGlobalPrefix('api/v1');
+  app.setGlobalPrefix(API_PREFIX);
   app.enableCors();
 
   const PORT = process.env.PORT ?? 5000;
@@ -17,6 +19,15 @@ async function bootstrap() {
       stopAtFirstError: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle(APP_NAME)
+    .setDescription(APP_NAME + ' API docs')
+    .setVersion('2.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup(`${API_PREFIX}/api-docs`, app, document);
 
   await app.listen(PORT);
 }
